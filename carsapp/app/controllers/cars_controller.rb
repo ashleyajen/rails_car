25b2f4803e3b654996846f94a48f50ac95bc2b46
Learn <learn@learn10.local>
2017-02-24 11:50:07 -0800

    pretesting commit

--- carsapp/app/controllers/cars_controller.rb
+++ carsapp/app/controllers/cars_controller.rb
@@ -3,16 +3,12 @@ class CarsController < ApplicationController
     @make = params[:make] unless params[:make].nil?
     @year = params[:year] unless params[:year].nil?
 
-    if  @make.nil? || !params.has_key?(:make) || params[:make].strip.empty?
-        flash.now[:alert] = "make missing"
-        render "sim.html.erb"
-    elsif @year.nil? || !params.has_key?(:year) || params[:year].strip.empty?
-        flash.now[:notice] = "year missing"
-        render "sim.html.erb"
-    elsif
+    if params.has_key?(:make) && !params[:make].strip.empty? &&
+       params.has_key?(:year) && !params[:year].strip.empty?
+
+      @ashley_car = CarSpecs.new(params[:make], params[:year])
       # save name in a cookie, so it can be accessed in the game
-      session[:make] = params[:make]
-      session[:year] = params[:year]
+      session[:ashley_car] = @ashley_car.to_yaml
       # redirect to the game play page
       redirect_to "/status"
     end
--- carsapp/app/models/car_specs.rb
+++ carsapp/app/models/car_specs.rb
@@ -1,5 +1,29 @@
 class CarSpecs
-def initialize (make, year)
-  @make = make
-  @year = year
+  def initialize (make, year)
+    @make = make
+    @year = year
+    @speed = 0
+  end
+
+  #to be called in the view
+  # def make_model
+  #   @make + " " + @year
+  # end
+
+  #necessary for YAML
+  def make #= new_make
+    @make #= new_make
+  end
+
+  def year #= new_year
+    @year #= new_year
+  end
+
+  def speed
+    @speed
+  end
+
+  def accelerate
+    @speed + 10
+  end
 end
